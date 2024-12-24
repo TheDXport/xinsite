c4cb9e53c9b86e74c02cf9a74163ba8f81119d47
TheDXport <therealdx10@gmail.com>
2024-12-24 00:53:50 -0500

    Section Link with Animations done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,15 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
       dropShadow: {
         "3xl": "0 0px 5px rgba(255, 255, 255, 0.5)",
         "4xl": ["0 35px 35px rgba(0, 0, 0, 0.25)", "0 45px 65px rgba(0, 0, 0, 0.15)"],
+      },
+      colors: {
+        background: "#0E0E0E",
+        passiveText: "#5c5c5c",
+        bingBG: "#7DFF86",
+        fdrBG: "#FF7D7D",
       },
     },
   },
