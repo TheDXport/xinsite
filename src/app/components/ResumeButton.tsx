7d1a10084beb760703ce4ee37e0aee11126425c4
TheDXport <therealdx10@gmail.com>
2024-12-25 07:29:52 -0500

    update

--- src/app/components/ResumeButton.tsx
+++ src/app/components/ResumeButton.tsx
@@ -10,7 +10,7 @@ const ResumeButton = () => {
       <a
         href="https://docs.google.com/document/d/1MV11f4aHe7YtEnTGa9xlRyzDvwbpjqkHLNh7iTsGnyo/edit?usp=sharing"
         className={`${DMMono.className} transform translate-all duration-500 pt-3 md:pt-4 pb-4 md:pb-5 px-6 md:px-9 text-md border-[1.5px] rounded-xl border-[#a2a1a3] text-[#dbdadc] hover:text-white hover:text-mdandlg `}>
-        get my resume
+        get resume
       </a>
     </div>
   );
