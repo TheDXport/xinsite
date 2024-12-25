7b11ab3d8ae82efb51c42c31ab25e5180ec39231
TheDXport <therealdx10@gmail.com>
2024-12-25 06:56:57 -0500

    test build for deployment

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,35 +2,28 @@ import localFont from "next/font/local";
 
 export const articulatExtraBold = localFont({
   src: "../../public/fonts/ArticulatExtrabold.otf",
-  display: "swap",
 });
 
 export const articulatMedium = localFont({
   src: "../../public/fonts/ArticulatMedium.otf",
-  display: "swap",
 });
 
 export const articulatNormal = localFont({
   src: "../../public/fonts/ArticulatNormal.otf",
-  display: "swap",
 });
 
 export const SFProBold = localFont({
   src: "../../public/fonts/SFPRODISPLAYBOLD.otf",
-  display: "swap",
 });
 
 export const SFProMedium = localFont({
   src: "../../public/fonts/SFPRODISPLAYMEDIUM.otf",
-  display: "swap",
 });
 
 export const SFProRegular = localFont({
   src: "../../public/fonts/SFPRODISPLAYREGULAR.otf",
-  display: "swap",
 });
 
 export const SFProLightItalic = localFont({
   src: "../../public/fonts/SFPRODISPLAYLIGHTITALIC.otf",
-  display: "swap",
 });
