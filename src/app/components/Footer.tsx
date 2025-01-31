6655f210050f994316236dbafc561f990fe68b36
Xin Dong <therealdx10@gmail.com>
2025-01-31 01:22:46 -0500

    updated footer year with function

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,8 +6,10 @@ const SFProRegular = localFont({
 const Footer = () => {
   return (
     <footer className={`${SFProRegular.className} mt-24 pt-20 `}>
-      <h1>&#169; 2024 Xin Dong. All rights reserved.</h1>
-      <h3 className="text-[#a3a3a3]">made with love and the loss of peeky</h3>{" "}
+      <h1>&#169; {new Date().getFullYear()} Xin Dong. All rights reserved.</h1>
+      <h3 className="text-[#a3a3a3]">
+        made with love and the loss of peeky
+      </h3>{" "}
     </footer>
   );
 };
